ef83bbeafa3c920ba7bf257c59695fca067dcfbd
Jangho <devjh1106@gmail.com>
2016-08-25 10:41:44 +0900

    Hello'

---
+++ CVTest/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using Tracker;
+
+namespace CVTest
+{
+    class Program
+    {
+        public static Rangef[] colorRanges = { new Rangef(0, 180), new Rangef(0, 256) };
+        public static string roiName = "iphone.jpg";
+
+        static void Main(string[] args)
+        {
+            ObjectTracker tracker = ObjectTracker.Instance;
+            Mat modelImage = Cv2.ImRead(roiName);
+
+            tracker.SetEntireArea(640, 480);
+            tracker.SetModelImageAndMakeHistogram(modelImage, new int[] { 0, 1 }, 2, new int[] { 180, 256 }, colorRanges);
+
+            VideoCapture streamer = new VideoCapture(0);
+            Rect meanShiftRect = new Rect();
+            meanShiftRect.X = 320;
+            meanShiftRect.Y = 240;
+
+            using (Window screen = new Window("screen"))
+            using (Mat eachFrame = new Mat())
+            {
+                while (true)
+                {
+                    streamer.Read(eachFrame);
+                    if (eachFrame.Empty())
+                    {
+                        break;
+                    }
+
+                    var result = tracker.TrackUsing(eachFrame, meanShiftRect.X, meanShiftRect.Y);
+
+                    screen.ShowImage(result.Frame);
+                    meanShiftRect.X = result.X;
+                    meanShiftRect.Y = result.Y;
+
+                    var key = Cv2.WaitKey(100);
+                    if (key == 27)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void WellTestedHSCapturing()
+        {
+            VideoCapture streamer = new VideoCapture(0);
+
+            using (Window testScreen = new Window("streamer"))
+            using (Mat roiMat = Cv2.ImRead(roiName))                 // read model image
+            using (Mat hsvRoiMat = roiMat.CvtColor(ColorConversionCodes.BGR2HSV))    // change format of model image
+            using (Mat hsvRoiMatHisto = new Mat())
+            using (Mat backProjectMat = new Mat())
+            using (Mat eachFrame = new Mat()) // Frame image buffer
+            {
+                // 모델히스토그램 만들기
+                Cv2.CalcHist(new Mat[] { hsvRoiMat }, new int[] { 0, 1 }, null, hsvRoiMatHisto, 2, new int[] { 180, 256 }, colorRanges);
+                Cv2.Normalize(hsvRoiMatHisto, hsvRoiMatHisto, 0, 255, NormTypes.MinMax);
+                Rect windowRect = new Rect(100, 100, roiMat.Width, roiMat.Height);
+
+                while (true)
+                {
+                    streamer.Read(eachFrame);
+                    if (eachFrame.Empty())
+                    {
+                        break;
+                    }
+
+                    using (Mat eachHSVFrame = eachFrame.CvtColor(ColorConversionCodes.BGR2HSV))
+                    {
+                        Cv2.CalcBackProject(new Mat[] { eachHSVFrame }, new int[] { 0, 1 }, hsvRoiMatHisto, backProjectMat, colorRanges);
+                    }
+
+                    Cv2.MeanShift(backProjectMat, ref windowRect, TermCriteria.Both(20, 1));
+                    Cv2.Rectangle(eachFrame, windowRect, 255, 3);
+
+                    testScreen.ShowImage(eachFrame);
+                    //testScreen.ShowImage(backProjectMat.CvtColor(ColorConversionCodes.GRAY2BGR));
+                    //testScreen.ShowImage(backProjectMat);
+                    var key = Cv2.WaitKey(100);
+
+                    if (key == 27)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void Testing1()
+        {
+
+            var frame = Cv2.ImRead("frame.jpg");
+            var hsvFrame = frame.CvtColor(ColorConversionCodes.BGR2HSV);
+
+            var roi = Cv2.ImRead("redBox.jpg");
+            var hsvRoi = roi.CvtColor(ColorConversionCodes.BGR2HSV);
+            var hsvRoiHist = new Mat();
+
+
+            Cv2.CalcHist(new Mat[] { hsvRoi }, new int[] { 0, 1 }, null, hsvRoiHist, 2, new int[] { 180, 256 }, colorRanges);
+            Cv2.Normalize(hsvRoiHist, hsvRoiHist, 0, 255, NormTypes.MinMax);
+
+            var backProjectMat = new Mat();
+
+            Cv2.CalcBackProject(new Mat[] { hsvFrame }, new int[] { 0, 1 }, hsvRoiHist, backProjectMat, colorRanges);
+            Cv2.ImShow("backproject image", backProjectMat);
+            Cv2.WaitKey(0);
+        }
+    }
+}
---
+++ Tracker/ObjectTracker.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using System.Diagnostics;
+
+namespace Tracker
+{
+    public class ObjectTracker
+    {
+        // static variables 
+        public static readonly Rangef[] HueSatColorRanges = { new Rangef(0, 180), new Rangef(0, 256) };
+        public static ObjectTracker Instance { get; }
+
+        public Point mInitialLocation = new Point(0, 0);
+
+        public int FindAreaWidth { get; private set; }
+        public int FindAreaHeight { get; private set; }
+
+        public Mat CvtModelImage { get; private set; } = null;
+        public Mat ModelHistogram { get; private set; } = null;
+        private bool IsFindAreaSettingDone { get; set; } = false;
+        private bool IsModelHistogramReady { get; set; } = false;
+
+        public int[] Channels { get; private set; } = null;
+        public int Dims { get; private set; } = 0;
+        public int[] HistSize { get; private set; } = null;
+        public Rangef[] ColorRanges { get; private set; } = null;
+
+        public bool IsTrackingReady
+        {
+            get
+            {
+                return (IsFindAreaSettingDone && IsModelHistogramReady);
+            }
+        }
+        static ObjectTracker()
+        {
+#if SHOW_LOG
+            Console.WriteLine("ObjectTracker 생성");
+#endif
+            Instance = new ObjectTracker();
+        }
+        public void SetEntireArea(int width, int height)
+        {
+            Debug.Assert(width > 0 && height > 0);
+            FindAreaWidth = width;
+            FindAreaHeight = height;
+
+            IsFindAreaSettingDone = true;
+        }
+
+        public void SetModelImageAndMakeHistogram(Mat modelImage, int[] channels, int dims, int[] histSize, Rangef[] colorRanges)
+        {
+            Debug.Assert(modelImage != null && modelImage.IsDisposed == false);
+            Debug.Assert(channels != null && channels.Length <= 3 && dims <= 3);
+            Debug.Assert(histSize != null && histSize.Length == dims);
+            Debug.Assert(colorRanges != null && colorRanges.Length == dims);
+            Debug.Assert(ModelHistogram == null);
+            Debug.Assert(IsFindAreaSettingDone == true);
+
+            // 모델 히스토그램 행렬 생성
+            ModelHistogram = new Mat();
+
+            // 모델이미지를 HSV 포맷으로 변환 -> 히스토그램 계산 -> 노멀라이즈
+            CvtModelImage = modelImage.CvtColor(ColorConversionCodes.BGR2HSV);
+            Cv2.CalcHist(new Mat[] { CvtModelImage }, channels, null, ModelHistogram, dims, histSize, colorRanges);
+            Cv2.Normalize(ModelHistogram, ModelHistogram, 0, 255, NormTypes.MinMax);
+
+            // 채널, 히스토그램 사이즈, 컬러범위 깊은 복사
+            Channels = new int[channels.Length];
+            HistSize = new int[histSize.Length];
+
+            // 차원 복사
+            Dims = dims;
+            for(int i = 0; i < channels.Length; ++i)
+            {
+                Channels[i] = channels[i];
+                HistSize[i] = histSize[i];
+            }
+            ColorRanges = colorRanges;
+
+            IsModelHistogramReady = true;
+        }
+        public TrackResult TrackUsing(Mat currentFrame, int initX, int initY)
+        {
+            Debug.Assert(IsTrackingReady == true);
+            Debug.Assert(currentFrame != null && currentFrame.IsDisposed == false);
+            Debug.Assert(Channels != null && Dims > 0 && HistSize != null && ColorRanges != null);
+            Debug.Assert((initX >= 0 && initX <= (FindAreaWidth - CvtModelImage.Width))
+                          && (initY >= 0 && initY <= (FindAreaHeight - CvtModelImage.Height)));
+
+            Mat backProjectMat = new Mat();
+            Rect windowRect = new Rect();
+
+            windowRect.X = initX;
+            windowRect.Y = initY;
+            windowRect.Width = CvtModelImage.Width;
+            windowRect.Height = CvtModelImage.Height;
+
+            using (Mat cvtCurrentFrame = currentFrame.CvtColor(ColorConversionCodes.BGR2HSV))
+            {
+                Cv2.CalcBackProject(new Mat[] { cvtCurrentFrame }, Channels, ModelHistogram, backProjectMat, ColorRanges);
+                Cv2.MeanShift(backProjectMat, ref windowRect, TermCriteria.Both(20, 1));
+                Cv2.Rectangle(currentFrame, windowRect, 255, 3);
+            }
+
+            return new TrackResult(windowRect.X, windowRect.Y, currentFrame);
+        }
+
+
+        public class TrackResult
+        {
+            public int X { get; private set; } 
+            public int Y { get; private set; }
+            public Mat Frame { get; }
+
+            public TrackResult(int x, int y, Mat frame)
+            {
+                Debug.Assert(x >= 0 && y >= 0 && frame != null);
+                X = x;
+                Y = y;
+                Frame = frame.Clone();
+            }
+        }
+        
+    }
+}
